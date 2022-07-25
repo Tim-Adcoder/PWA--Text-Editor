c7cdb51cc215f3f41ce0901af1489fc4380d2001
TimS <92215345+Tim-Adcoder@users.noreply.github.com>
2022-07-25 23:57:54 +0930

    update webpack file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,17 +19,58 @@ module.exports = () => {
     },
     plugins: [
       //webpack plugin to add index.html that link to JS
+      new HtmlWebpackPlugin({
+				template: "./index.html",
+				title: "JATE",
+			}),
 
-      //service worker
+      //customed service worker
+      new InjectManifest({
+				swSrc: "./src-sw.js",
+				swDest: "src-sw.js",
+      }), 
 
       //create a manifest.json file
+      new WebpackPwaManifest({
+				fingerprints: false,
+				inject: true,
+				name: "My Text Editor",
+				short_name: "JATE",
+				description: "Text Editor with offline capabilities using IndexedDB",
+				background_color: "#225ca3",
+				theme_color: "#225ca3",
+				start_url: "/",
+				publicPath: "/",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join("assets", "icons"),
+					},
+				],
+			}),
     ],
 
     module: {
       rules: [
         //css loaders
-
+        {
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+				},
         //babel-loader to translate JS ES6
+        {
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					// We use babel-loader in order to use ES6.
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"],
+						},
+					},
+				},
       ],
     },
   };
